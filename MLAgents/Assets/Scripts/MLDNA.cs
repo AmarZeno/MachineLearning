a85e76f0cb5afd8f8a3f9c6e4fb3dc219d474eb1
Zeno Saviour <zeno.saviour@utah.edu>
2018-04-07 19:36:12 -0600

    Genetic algorithm logic added

---
+++ MLAgents/Assets/Scripts/MLDNA.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLDNA : MonoBehaviour {
+
+    // Gene for color
+    public float R, G, B;
+    public float TimeToDie = 0;
+
+    bool dead = false;
+    SpriteRenderer AgentSpriteRenderer;
+    Collider2D AgentCollider;
+
+	// Use this for initialization
+	void Start () {
+        AgentSpriteRenderer = GetComponent<SpriteRenderer>();
+        AgentCollider = GetComponent<Collider2D>();
+        AgentSpriteRenderer.color = new Color(R, G, B);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        dead = true;
+        TimeToDie = MLPopulationManager.elapsed;
+        Debug.Log("Dead at : " + TimeToDie);
+        AgentSpriteRenderer.enabled = false;
+        AgentCollider.enabled = false;
+    }
+}
---
+++ MLAgents/Assets/Scripts/MLPopulationManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MLPopulationManager : MonoBehaviour {
+
+    public GameObject AgentPrefab;
+    public int PopulationSize = 10;
+
+    List<GameObject> Population = new List<GameObject>();
+    public static float elapsed = 0;
+
+    int trialTime = 10;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        guiStyle.fontSize = 50;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 100, 20), "Generation: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 65, 100, 20), "Trial Time: " + (int)elapsed, guiStyle);
+    }
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < PopulationSize; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+            GameObject AgentObject = Instantiate(AgentPrefab, pos, Quaternion.identity);
+            AgentObject.GetComponent<MLDNA>().R = Random.Range(0.0f, 1.0f);
+            AgentObject.GetComponent<MLDNA>().G = Random.Range(0.0f, 1.0f);
+            AgentObject.GetComponent<MLDNA>().B = Random.Range(0.0f, 1.0f);
+            Population.Add(AgentObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsed += Time.deltaTime;
+        if(elapsed > trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+	}
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> NewPopulation = new List<GameObject>();
+        List<GameObject> SortedList = Population.OrderByDescending(o => o.GetComponent<MLDNA>().TimeToDie).ToList();
+
+        Population.Clear();
+
+        // Breed upper half of sorted list
+        for(int i = (int)(SortedList.Count/ 2.0f) - 1; i < SortedList.Count - 1; i++)
+        {
+            Population.Add(Breed(SortedList[i], SortedList[i + 1]));
+            Population.Add(Breed(SortedList[i + 1], SortedList[i]));
+        }
+
+        for(int i = 0; i < SortedList.Count; i++)
+        {
+            Destroy(SortedList[i]);
+        }
+
+        generation++;
+    }
+
+    GameObject Breed(GameObject ParentA, GameObject ParentB)
+    {
+        Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+        GameObject offspring = Instantiate(AgentPrefab, pos, Quaternion.identity);
+
+        MLDNA DNA1 = ParentA.GetComponent<MLDNA>();
+        MLDNA DNA2 = ParentB.GetComponent<MLDNA>();
+
+        // God of the swapping system for genetic dna
+        //Add Mutation
+        if (Random.Range(0, 1000) > 5)
+        {
+            offspring.GetComponent<MLDNA>().R = Random.Range(0, 10) < 5 ? DNA1.R : DNA2.R;
+            offspring.GetComponent<MLDNA>().G = Random.Range(0, 10) < 5 ? DNA1.G : DNA2.G;
+            offspring.GetComponent<MLDNA>().B = Random.Range(0, 10) < 5 ? DNA1.B : DNA2.B;
+        }
+        else
+        {
+            offspring.GetComponent<MLDNA>().R = Random.Range(0.0f, 1.0f);
+            offspring.GetComponent<MLDNA>().G = Random.Range(0.0f, 1.0f);
+            offspring.GetComponent<MLDNA>().B = Random.Range(0.0f, 1.0f);
+        }
+        return offspring;
+    }
+}

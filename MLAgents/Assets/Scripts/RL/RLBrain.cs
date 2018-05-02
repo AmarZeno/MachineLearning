c7fa394955adb37aef06466e340539bc6754128a
Zeno Saviour <amarzeno@outlook.com>
2018-05-02 00:42:41 -0600

    Added brain comps + ann for the pong game

--- MLAgents/Assets/Scripts/RL/RLBrain.cs
+++ MLAgents/Assets/Scripts/RL/RLBrain.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RLBrain : MonoBehaviour {
-
     public GameObject paddle;
     public GameObject ball;
     Rigidbody2D brb;
@@ -16,14 +15,64 @@ public class RLBrain : MonoBehaviour {
 
     ANN ann;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         ann = new ANN(6, 1, 1, 4, 0.11);
         brb = ball.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    List<double> Run(double bx, double by, double bvx, double bvy, double px, double py, double pv, bool train)
+    {
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+        inputs.Add(bx);
+        inputs.Add(by);
+        inputs.Add(bvx);
+        inputs.Add(bvy);
+        inputs.Add(px);
+        inputs.Add(py);
+        outputs.Add(pv);
+        if (train)
+            return (ann.Train(inputs, outputs));
+        else
+            return (ann.CalcOutput(inputs, outputs));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posy = Mathf.Clamp(paddle.transform.position.y + (yvel * Time.deltaTime * paddleMaxSpeed),
+                                 paddleMinY, paddleMaxY);
+        paddle.transform.position = new Vector3(paddle.transform.position.x, posy, paddle.transform.position.z);
+
+        List<double> output = new List<double>();
+        int layerMask = 1 << 10;
+        RaycastHit2D hit = Physics2D.Raycast(ball.transform.position, brb.velocity, 1000, layerMask);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "tops") //reflect off top
+            {
+                Vector3 reflection = Vector3.Reflect(brb.velocity, hit.normal);
+                hit = Physics2D.Raycast(hit.point, reflection, 1000, layerMask);
+            }
+
+            if (hit.collider != null && hit.collider.gameObject.tag == "backwall")
+            {
+                float dy = (hit.point.y - paddle.transform.position.y);
+
+                output = Run(ball.transform.position.x,
+                                    ball.transform.position.y,
+                                    brb.velocity.x, brb.velocity.y,
+                                    paddle.transform.position.x,
+                                    paddle.transform.position.y,
+                                    dy, true);
+                yvel = (float)output[0];
+            }
+
+        }
+        else
+            yvel = 0;
+    }
 }
